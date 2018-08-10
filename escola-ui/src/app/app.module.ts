306f68e36a6fdf642f54df54dbe981f245d9e012
brunobucard <brunobucard@gmail.com>
2018-08-10 16:58:46 -0300

    Tela aluno

--- escola-ui/src/app/app.module.ts
+++ escola-ui/src/app/app.module.ts
@@ -1,14 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { AlunoCadastroComponent } from './aluno-cadastro/aluno-cadastro.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    AlunoCadastroComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
