c79d5ff04d2edecfff3fa68d75d50901a002ac8c
brunobucard <brunobucard@gmail.com>
2018-08-14 17:53:45 -0300

    Aluno Mensagem validacao

--- escola-ui/src/app/app.module.ts
+++ escola-ui/src/app/app.module.ts
@@ -25,12 +25,14 @@ import {InputTextModule} from 'primeng/inputtext';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { AlunoCadastroComponent } from './aluno-cadastro/aluno-cadastro.component';
+import { MessageComponent } from './message/message.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    AlunoCadastroComponent
+    AlunoCadastroComponent,
+    MessageComponent
   ],
   imports: [
     BrowserModule,
