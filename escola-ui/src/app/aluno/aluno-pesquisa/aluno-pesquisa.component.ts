0a80674fa3e40e2e2c61f0cb4b5e1a661d37193f
brunobucard <brunobucard@gmail.com>
2018-11-21 21:50:00 -0200

    aluno-pesquisa

--- escola-ui/src/app/aluno/aluno-pesquisa/aluno-pesquisa.component.ts
+++ escola-ui/src/app/aluno/aluno-pesquisa/aluno-pesquisa.component.ts
@@ -1,21 +1,74 @@
-import { Component, OnInit } from '@angular/core';
+import { MessageService } from 'primeng/components/common/messageservice';
+import { ConfirmationService, LazyLoadEvent } from 'primeng/api';
+import { ErrorHandlerService } from './../../core/error-handler.service';
+import { AlunoFiltro, AlunoService } from './../aluno.service';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { AlunoGridComponent } from '../aluno-grid/aluno-grid.component';
+import { Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-aluno-pesquisa',
   templateUrl: './aluno-pesquisa.component.html',
   styleUrls: ['./aluno-pesquisa.component.css']
 })
-export class AlunoPesquisaComponent {
+export class AlunoPesquisaComponent implements OnInit {
 
-  alunos = [
-    { nome: 'Bruno', sexo: 'Masculino'},
-    { nome: 'Ana Elisa', sexo: 'Feminino'},
-    { nome: 'Renata', sexo: 'Feminino'},
-    { nome: 'João', sexo: 'Masculino'},
-    { nome: 'Ricardo', sexo: 'Masculino'},
-    { nome: 'Elvis', sexo: 'Masculino'},
-    { nome: 'Fred', sexo: 'Masculino'}
+  totalRegistros = 0;
+  filtro = new AlunoFiltro();
+  alunos = [];
+  @ViewChild('tabela') grid;
 
-  ];
+  constructor(
+    private alunoService: AlunoService,
+    private errorHandler: ErrorHandlerService,
+    private confirmation: ConfirmationService,
+    private messageService: MessageService,
+    private title: Title
+  ) { }
+
+  ngOnInit() {
+    this.title.setTitle('Pesquisa de alunos');
+  }
+
+  pesquisar(pagina = 0) {
+    this.filtro.pagina = pagina;
+
+    this.alunoService.pesquisar(this.filtro)
+      .then(resultado => {
+        this.totalRegistros = resultado.total;
+        this.alunos = resultado.alunos;
+      })
+      .catch(erro => this.errorHandler.handle(erro));
+  }
+
+  aoMudarPagina(event: LazyLoadEvent) {
+    const pagina = event.first / event.rows;
+    this.pesquisar(pagina);
+  }
+
+  confirmarExclusao(aluno: any) {
+    this.confirmation.confirm({
+      message: 'Tem certeza que deseja excluir?',
+      accept: () => {
+        this.excluir(aluno);
+      }
+    });
+  }
+
+  excluir(aluno: any) {
+    this.alunoService.excluir(aluno.codigo)
+      .then(() => {
+        if (this.grid.first === 0) {
+          this.pesquisar();
+        } else {
+          this.grid.first = 0;
+        }
+
+        this.messageService.add({ severity: 'success', detail: 'Aluno excluído com sucesso!'});
+      })
+      .catch(erro => this.errorHandler.handle(erro));
+  }
+
+
 
 }
--- escola-ui/src/app/aluno/aluno.service.ts
+++ escola-ui/src/app/aluno/aluno.service.ts
@@ -1,3 +1,4 @@
+import { Aluno } from './../core/model';
 import { HttpParams, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { MoneyHttp } from '../seguranca/money-http';
@@ -22,4 +23,58 @@ export class AlunoService {
   constructor(private http: MoneyHttp) {
     this.alunosUrl = `${environment.apiUrl}/alunos`;
   }
+
+  pesquisar(filtro: AlunoFiltro): Promise<any> {
+    let params = new HttpParams({
+      fromObject: {
+        page: filtro.pagina.toString(),
+        size: filtro.itensPorPagina.toString()
+      }
+    });
+
+    if (filtro.nome) {
+      params = params.append('nome', filtro.nome);
+    }
+
+    return this.http.get<any>(`${this.alunosUrl}`, { params })
+      .toPromise()
+      .then(response => {
+        const alunos = response.content;
+
+        const resultado = {
+          alunos,
+          total: response.totalElements
+        };
+
+        return resultado;
+      });
+  }
+
+  listarTodas(): Promise<any> {
+    return this.http.get<any>(this.alunosUrl)
+      .toPromise()
+      .then(response => response.content);
+  }
+
+  excluir(codigo: number): Promise<void> {
+    return this.http.delete(`${this.alunosUrl}/${codigo}`)
+      .toPromise()
+      .then(() => null);
+  }
+
+  adicionar(aluno: Aluno): Promise<Aluno> {
+    return this.http.post<Aluno>(this.alunosUrl, aluno)
+      .toPromise();
+  }
+
+  atualizar(aluno: Aluno): Promise<Aluno> {
+    return this.http.put<Aluno>(`${this.alunosUrl}/${aluno.codigo}`, aluno)
+      .toPromise();
+  }
+
+  buscarPorCodigo(codigo: number): Promise<Aluno> {
+    return this.http.get<Aluno>(`${this.alunosUrl}/${codigo}`)
+      .toPromise();
+  }
+
 }
