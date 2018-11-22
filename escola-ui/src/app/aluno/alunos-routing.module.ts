164b1f16e2d79d0b38c27ace0e70b0ba9222baf8
brunobucard <brunobucard@gmail.com>
2018-11-22 00:20:45 -0200

    aluno cadastro rota

--- escola-ui/src/app/aluno/alunos-routing.module.ts
+++ escola-ui/src/app/aluno/alunos-routing.module.ts
@@ -3,6 +3,7 @@ import { Routes, RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
 
 import { AuthGuard } from './../seguranca/auth.guard';
+import { AlunoCadastroComponent } from './aluno-cadastro/aluno-cadastro.component';
 
 
 const routes: Routes = [
@@ -11,7 +12,14 @@ const routes: Routes = [
     component: AlunoPesquisaComponent,
     canActivate: [AuthGuard],
     data: { roles: ['ROLE_PESQUISAR_ALUNO'] }
-  }
+  },
+  {
+    path: 'novo',
+    component: AlunoCadastroComponent,
+    canActivate: [AuthGuard],
+    data: { roles: ['ROLE_CADASTRAR_ALUNO'] }
+  },
+
 ];
 
 @NgModule({
