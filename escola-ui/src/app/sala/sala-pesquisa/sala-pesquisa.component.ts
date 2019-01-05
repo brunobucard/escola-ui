380eb7f511b61e13a4c9170e7c35041b153b702f
brunobucard <brunobucard@gmail.com>
2019-01-04 23:53:00 -0200

    sala pesquisa

--- escola-ui/src/app/sala/sala-pesquisa/sala-pesquisa.component.ts
+++ escola-ui/src/app/sala/sala-pesquisa/sala-pesquisa.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { ConfirmationService, MessageService, LazyLoadEvent } from 'primeng/api';
+import { SalaFiltro, SalaService } from './../sala.service';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { ErrorHandlerService } from 'src/app/core/error-handler.service';
+import { Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-sala-pesquisa',
@@ -7,9 +11,62 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SalaPesquisaComponent implements OnInit {
 
-  constructor() { }
+  totalRegistros = 0;
+  filtro = new SalaFiltro();
+  salas = [];
+  @ViewChild('tabela') grid;
+
+  constructor(
+    private salaService: SalaService,
+    private errorHandler: ErrorHandlerService,
+    private confirmation: ConfirmationService,
+    private messageService: MessageService,
+    private title: Title
+  ) { }
 
   ngOnInit() {
+    this.title.setTitle('Pesquisa de salas');
   }
+
+  pesquisar(pagina = 0) {
+    this.filtro.pagina = pagina;
+
+    this.salaService.pesquisar(this.filtro)
+      .then(resultado => {
+        this.totalRegistros = resultado.total;
+        this.salas = resultado.salas;
+      })
+      .catch(erro => this.errorHandler.handle(erro));
+  }
+
+  aoMudarPagina(event: LazyLoadEvent) {
+    const pagina = event.first / event.rows;
+    this.pesquisar(pagina);
+  }
+
+  confirmarExlusao(sala: any) {
+    this.confirmation.confirm({
+      message: 'Tem certeza que deseja excluir?',
+      accept: () => {
+        this.excluir(sala);
+      }
+    });
+  }
+
+  excluir(sala: any) {
+    this.salaService.excluir(sala.codigo)
+      .then(() => {
+        if (this.grid.first === 0) {
+          this.pesquisar();
+        } else {
+          this.grid.first = 0;
+        }
+
+        this.messageService.add({ severity: 'success', detail: 'Turma excluída com sucesso!'});
+      })
+      .catch(erro => this.errorHandler.handle(erro));
+  }
+
+
 
 }
--- escola-ui/src/app/sala/sala.service.ts
+++ escola-ui/src/app/sala/sala.service.ts
@@ -1,3 +1,4 @@
+import { Sala } from './../core/model';
 import { Injectable } from '@angular/core';
 
 import { Http } from '@angular/http';
@@ -28,5 +29,62 @@ export class SalaService {
     this.salasUrl = `${environment.apiUrl}/salas`;
    }
 
+   pesquisar(filtro: SalaFiltro): Promise<any> {
+    let params = new HttpParams({
+      fromObject: {
+        page: filtro.pagina.toString(),
+        size: filtro.itensPorPagina.toString()
+      }
+    });
+
+    if (filtro.sala) {
+      params = params.append('sala', filtro.sala);
+    }
+
+    return this.http.get<any>(`${this.salasUrl}`, { params })
+      .toPromise()
+      .then(response => {
+        const salas = response.content;
+
+        const resultado = {
+          salas,
+          total: response.totalElements
+        };
+
+        return resultado;
+      });
+  }
+
+   listarTodas(): Promise<any> {
+     return this.http.get<any>(this.salasUrl)
+      .toPromise()
+      .then(response => response.content);
+   }
+
+   excluir(codigo: number): Promise<void> {
+     return this.http.delete(`${this.salasUrl}/${codigo}`)
+      .toPromise()
+      .then(() => null);
+   }
+
+   adicionar(sala: Sala): Promise<Sala> {
+     return this.http.post<Sala>(this.salasUrl, sala)
+      .toPromise();
+   }
+
+   atualizar(sala: Sala): Promise<Sala> {
+     return this.http.put<Sala>(`${this.salasUrl}/${sala.codigo}`, sala)
+      .toPromise();
+   }
+
+   buscarPorCodigo(codigo: number): Promise<Sala> {
+     return this.http.get<Sala>(`${this.salasUrl}/${codigo}`)
+      .toPromise()
+      .then(response => {
+        const sala = response;
+
+        return sala;
+      });
+   }
 
 }
